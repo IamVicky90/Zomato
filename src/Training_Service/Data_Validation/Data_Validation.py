3fa69d67d7d0e3bb3ac034cc4912ec2cb44fca1e
Waqas Bilal <waqasbilal02@gmail.com>
2021-06-09 11:35:42 +0500

    Name Validation Part Completed

--- src/Training_Service/Data_Validation/Data_Validation.py
+++ src/Training_Service/Data_Validation/Data_Validation.py
@@ -16,20 +16,28 @@ class Validate_data:
                 name,_=file.split('.')
                 _,LengthOfDateStampInFile, LengthOfTimeStampInFile=name.split('_')
                 if len(LengthOfDateStampInFile)==int(self.json_file['LengthOfDateStampInFile']) and len(LengthOfTimeStampInFile)==int(self.json_file['LengthOfTimeStampInFile']):
-                    self.log.log_writer(f'LengthOfDateStampInFile and LengthOfTimeStampInFile,  Matches for file {file}','Data_Validation.log')
-                    self.move_Good_Data_Folder(os.path.join('Training_Batch_Files',file),file)
+                    self.log.log_writer(f'LengthOfDateStampInFile and LengthOfTimeStampInFile,  Matches for file {file} and copy to Good Data Folder','Data_Validation.log')
+                    self.copy_Good_Data_Folder(os.path.join('Training_Batch_Files',file),file)
                 else:
-                    self.log.log_writer(f'LengthOfDateStampInFile and LengthOfTimeStampInFile,  not Matches for file {file}','Data_Validation.log',message_type='Warning')
+                    self.log.log_writer(f'LengthOfDateStampInFile and LengthOfTimeStampInFile,  not Matches for file {file} and Copy to Bad Data Folder','Data_Validation.log',message_type='Warning')
+                    self.copy_Bad_Data_Folder(os.path.join('Training_Batch_Files',file),file)
             else:
-                self.log.log_writer(f'Regex not matches for file {file}','Data_Validation.log','Warning')
+                self.log.log_writer(f'Regex not matches for file {file} and copy to Bad Data Folder','Data_Validation.log','Warning')
+                self.copy_Bad_Data_Folder(os.path.join('Training_Batch_Files',file),file)
 
         return
-    def move_Good_Data_Folder(self,file_path,file):
+    def copy_Good_Data_Folder(self,file_path,file):
         os.makedirs('Good_Data_Folder',exist_ok=True)
         try:
             shutil.move(file_path,'Good_Data_Folder')
             self.log.log_writer(f'Sucessfully copy the file {file} to Good_Data_Folder','Data_Validation.log')
         except Exception as e:
             self.log.log_writer(f'Could not copy the file {file} to Good_Data_Folder error: {str(e)}','Data_Validation.log','Error')
-    def copy(self):
-        pass
+    def copy_Bad_Data_Folder(self,file_path,file):
+        os.makedirs('Good_Data_Folder',exist_ok=True)
+        try:
+            shutil.move(file_path,'Bad_Data_Folder')
+            self.log.log_writer(f'Sucessfully copy the file {file} to Bad_Data_Folder','Data_Validation.log')
+        except Exception as e:
+            self.log.log_writer(f'Could not copy the file {file} to Bad_Data_Folder error: {str(e)}','Data_Validation.log','Error')
+   
